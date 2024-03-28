7690d7439f3943f11275c9a941f571728b0b8ae7
mkoner <amzid@hotmail.fr>
2024-03-28 18:03:15 -0500

    Internationalization sing MessageSource

--- src/main/java/com/mkoner/spring_framework_core/using_xml_config/Test.java
+++ src/main/java/com/mkoner/spring_framework_core/using_xml_config/Test.java
@@ -1,5 +1,8 @@
 package com.mkoner.spring_framework_core.using_xml_config;
 
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,6 +16,17 @@ public class Test {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Computer computer1 = context.getBean("computer", Computer.class);
 		System.out.println(computer1);
+		Locale english = Locale.ENGLISH;
+		Locale french = Locale.FRENCH;
+		Object[] object =  new Object[] {"Mamadou", new Date()};
+		System.out.println("================ENGLISH==========");
+		System.out.println(context.getMessage("msg", null, english));
+		System.out.println(context.getMessage("paramMesg", object, english));
+		
+		System.out.println("================FRENCH==========");
+		System.out.println(context.getMessage("msg", null, french));
+		System.out.println(context.getMessage("paramMesg", object, french));
+		
 		((ClassPathXmlApplicationContext)context).close();
 		
 	}
